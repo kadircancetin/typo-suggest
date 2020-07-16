df6a0587473b7d8e2c1608814059128b94afbef8
kadir can çetin <kadircancetin@gmail.com>
2020-07-16 17:46:01 +0300

    Fix doc problems on code with M-x checkdoc

--- typo-suggest.el
+++ typo-suggest.el
@@ -1,5 +1,4 @@
-;; -*- lexical-binding: t -*-
-;;; typo-suggest.el --- Don't make typo with the help of helm and compny
+;;; typo-suggest.el --- Don't make typo with the help of helm and compny ;; -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2020  Kadir Can Çetin
 
@@ -42,7 +41,7 @@
   :group 'helm)
 
 (defcustom typo-suggest-suggestion-count 20
-  "Number of suggestion for 'helm and company completion"
+  "Number of suggestion for 'helm and company completion."
   :type 'integer
   :group 'typo-suggest)
 
@@ -54,7 +53,8 @@
 
 
 (defun typo-suggest--fetch-results (query)
-  "Fetching results from datamuse api and returns as a string"
+  "Fetching results from datamuse api and return as a string.
+Argument QUERY is string which will searched."
   (with-current-buffer
       ;; TODO: show error or something to user if bigger than 'typo-suggest-timout sec
       (url-retrieve-synchronously
@@ -66,21 +66,23 @@
     (delete-region (point)(point-min))(buffer-string)))
 
 (defun typo-suggest--results (fetched_str)
-  "Gets json but string datatype result of the datamuse api,
-returns list of strings suggestion."
+  "Gets json str, return parsed elisp obj.
+It returns list of strings suggestion.  Argument FETCHED_STR is
+comes from `typo-suggest--fetch-result'."
+
   (mapcar 'cdr (mapcar 'car (json-read-from-string  (typo-suggest--fetch-results fetched_str)))))
 
 
 ;;;###autoload
 (defun typo-suggest--helm-replace-word(x)
-  "Replace the word under the cursor with x paramter."
+  "Replace the word under the cursor with X paramter."
   (interactive)
   (save-excursion
     (delete-region (beginning-of-thing 'word) (end-of-thing 'word))
     (insert x)))
 
 (defun typo-suggest--do-helm(input)
-  "Strating helm suggestion with input parameter."
+  "Strating helm suggestion with INPUT parameter."
   (helm :sources
         (helm-build-sync-source "Helm Word"
           :candidates (lambda (&optional _) (typo-suggest--results helm-input))
@@ -107,7 +109,10 @@ returns list of strings suggestion."
 
 ;;;###autoload
 (defun typo-suggest-company (command &optional arg &rest ignored)
-  "Get word suggestion from datamuse api with company mode."
+  "Get word suggestion from datamuse api with company mode.
+Argument COMMAND is used for company.
+Optional argument ARG Is used from company to send which will search.
+Optional argument IGNORED ignored arguments."
   (interactive (list 'interactive))
   (cl-case command
     (interactive (company-begin-backend 'typo-suggest-company))
@@ -121,4 +126,4 @@ returns list of strings suggestion."
 
 (provide 'typo-suggest)
 
-;; typo-suggest.el ends here
+;;; typo-suggest.el ends here
